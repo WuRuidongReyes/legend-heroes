fe3816d7d515ff9fa86f7f8d261a546c82ffda18
WuRuidongReyes <963211782@qq.com>
2019-10-24 16:18:39 +0800

    英雄详情

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       HomePageComponent,
-      HeroListComponent
+      HeroListComponent,
+      HeroDetailComponent
    ],
    imports: [
       BrowserModule,
