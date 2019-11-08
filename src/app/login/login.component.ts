a4ef82dc7eac84eb0e41ed7440552996f0a4340d
WuRuidongReyes <963211782@qq.com>
2019-11-08 15:15:28 +0800

    验证表单输入的完整性

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,9 +9,27 @@ export class LoginComponent implements OnInit {
 
   userID = '';
   password = '';
+  errorHint = '';
   constructor() { }
 
   ngOnInit() {
   }
 
+  cleanID() {
+    this.userID = '';
+  }
+
+  login() {
+    if (this.userID === '') {
+      this.errorHint = '账号不能为空';
+      document.getElementById('id').focus();
+      return;
+    }
+    if (this.password === '') {
+      this.errorHint = '密码不能为空';
+      document.getElementById('password').focus();
+      return;
+    }
+    this.errorHint = '';
+  }
 }
