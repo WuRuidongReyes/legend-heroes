b7650557850bdf176281a5c33d1acdc14717e708
WuRuidongReyes <963211782@qq.com>
2019-11-11 18:17:44 +0800

    背景设置

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,25 +10,45 @@ export class LoginComponent implements OnInit {
   userID = '';
   password = '';
   errorHint = '';
-  imageURL = '../../assets/images/login/senna_1920_1200.jpg';
+  imageURL = [
+    '../../assets/images/login/senna_1920_1200.jpg',
+    '../../assets/images/login/64138434_p0.jpg'
+  ];
+  i = 0;
   constructor() { }
 
   ngOnInit() {
+    setInterval(() => {
+      // document.getElementById('container').style.backgroundImage = `url(${this.imageURL[this.i]})`;
+      // this.i++;
+      // this.i %= this.imageURL.length;
+      // console.log(this.i);
+    }, 2000);
+    document.getElementById('container').style.backgroundImage = `url(${this.imageURL[0]})`;
+    console.log(window.innerWidth);
+    console.log(window.outerWidth);
   }
 
   cleanID() {
     this.userID = '';
   }
+  cleanErrorHint() {
+    this.errorHint = '';
+  }
 
   login() {
     if (this.userID === '') {
       this.errorHint = '账号不能为空';
-      document.getElementById('id').focus();
+      const inputID = document.getElementById('id');
+      inputID.focus();
+      inputID.addEventListener('input', this.cleanErrorHint);
       return;
     }
     if (this.password === '') {
       this.errorHint = '密码不能为空';
-      document.getElementById('password').focus();
+      const inputPW = document.getElementById('password');
+      inputPW.focus();
+      inputPW.addEventListener('input', this.cleanErrorHint);
       return;
     }
     this.errorHint = '';
