08b1e90cf9a60c40db0fe0a04bfd698193645cec
WuRuidongReyes <963211782@qq.com>
2019-11-13 10:19:44 +0800

    包入动画模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { LoginComponent } from './login/login.component';
       BrowserModule,
       AppRoutingModule,
       FormsModule,
+      BrowserAnimationsModule
    ],
    providers: [],
    bootstrap: [
